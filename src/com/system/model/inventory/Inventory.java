dfc396cb3d927f0319e8b79ed9e3253cfc404d29
Thomas Evans <tevans6@sheffield.ac.uk>
2023-11-06 20:00:44 +0000

    increaseStock
    Did the method but not working with gui yet.

--- src/com/system/model/inventory/Inventory.java
+++ src/com/system/model/inventory/Inventory.java
@@ -1,5 +1,6 @@
 package com.system.model.inventory;
 import com.system.model.user.*;
+import java.sql.*;
 
 public class Inventory {
 
@@ -14,9 +15,53 @@ public class Inventory {
         }
     }
 
-    public void increaseStock() {
-        checkStaffRole();
-        // TODO - implement Inventory.increaseStock
+    public static void increaseStock(String productCode, int newQuantity) {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        
+        try {
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://stusql.dcs.shef.ac.uk/team015", "team015", "eSh7Shahk");
+        
+            String updateQuery = "UPDATE `inventory` SET quantity = ? WHERE product_code = ?";
+            pstmt = con.prepareStatement(updateQuery);
+            pstmt.setInt(1, newQuantity);
+            pstmt.setString(2, productCode);
+            int rowsUpdated = pstmt.executeUpdate();
+
+            if (rowsUpdated > 0) {
+                System.out.println("Stock updated successfully.");
+            } else {
+                System.out.println("Product not found or no changes made.");
+            } 
+        }
+        catch (SQLException ex) { // Catch SQLException
+            ex.printStackTrace();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            try {
+                if (pstmt != null) 
+                    pstmt.close();
+                if (con != null) 
+                    con.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+        
+        
+        
+        
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            if (con != null) con.close();
+        }
     }
 
     public void decreaseStock() {
