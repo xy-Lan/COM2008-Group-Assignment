c046c301f0de8b8fd2ebe018ced6b6acd80a7e90
YU LIN <ylin122@sheffield.ac.uk>
2023-11-21 16:33:00 +0000

    Implement a function that list out the train sets

--- src/main/java/project/daoimpl/TrainSetDaoImpl.java
+++ src/main/java/project/daoimpl/TrainSetDaoImpl.java
@@ -5,6 +5,7 @@ import project.dao.TrainSetDao;
 import project.model.product.TrainSet;
 import project.service.MysqlService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.sql.*;
 
@@ -30,8 +31,26 @@ public class TrainSetDaoImpl implements TrainSetDao {
 
     @Override
     public List<TrainSet> getAllTrainSets() {
-        // Implement logic to retrieve all train sets from the database
-        return null;
+        //Implement logic to retrieve all train sets from the database
+        List<TrainSet> trainSets = new ArrayList<>();
+        String sql = "SELECT ts.product_code, p.product_name, p.brand_name, p.retail_price " +
+                "FROM train_set ts JOIN product p ON ts.product_code = p.product_code";
+        try (Connection conn = mysqlService.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                TrainSet trainSet = new TrainSet();
+                trainSet.setProductCode(rs.getString("product_code"));
+                trainSet.setProductName(rs.getString("product_name"));
+                trainSet.setBrandName(rs.getString("brand_name"));
+                trainSet.setRetailPrice(rs.getBigDecimal("retail_price"));
+                trainSets.add(trainSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return trainSets;
     }
 
     @Override
