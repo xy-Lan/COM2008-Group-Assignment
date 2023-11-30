47b4ef0dd6087d4eeb8a404dd053d5973f982680
xy-Lan <lanxingy2001@gmail.com>
2023-11-30 20:40:45 +0000

    updateInventoryForOrder

--- src/main/java/project/service/InventoryService.java
+++ src/main/java/project/service/InventoryService.java
@@ -4,6 +4,11 @@ import java.sql.*;
 import java.util.List;
 import java.util.logging.Level;
 
+import project.dao.OrderLineDao;
+import project.daoimpl.InventoryDaoImpl;
+import project.daoimpl.OrderLineDaoImpl;
+import project.model.order.Order;
+import project.model.order.OrderLine;
 import project.model.user.*;
 
 import project.dao.InventoryDao;
@@ -12,20 +17,19 @@ import project.model.user.Role;
 
 public class InventoryService {
 
-    private InventoryDao inventoryDao;
+    private InventoryDao inventoryDao = new InventoryDaoImpl();
 
     private User updater;
 
     private List<Inventory> inventories;
+    private OrderLineDao orderLineDao = new OrderLineDaoImpl();
 
-    // Additional parameters may be required
     private void checkStaffRole() {
         if (!updater.hasRole(Role.STAFF)) {
             throw new UnsupportedOperationException("Only staff can perform this operation.");
         }
     }
 
-    // Additional parameters may be required
     public Boolean increaseStock(String productCode, int newShipmentQuantity) {
         checkStaffRole();
 
@@ -71,9 +75,23 @@ public class InventoryService {
         return null;
     }
 
+    public void updateInventoryForOrder(Order order) throws SQLException {
+        List<OrderLine> orderLines = orderLineDao.getAllOrderLines(order.getOrderNumber());
+
+        for (OrderLine orderLine : orderLines) {
+            String productCode = orderLine.getProductCode();
+            int quantity = orderLine.getQuantity();
+
+            inventoryDao.decreaseStock(productCode, quantity);
+        }
+    }
+
     // Additional parameters may be required
     public void checkStock() {
         checkStaffRole();
         // TODO - implement Inventory.checkStock
     }
+
+
+
 }
